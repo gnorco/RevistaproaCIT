31b3fa5ccd19541e5f8ddc1eeb5b42429c1a7fb0
gnorco <gnardironco@escuelasproa.edu.ar>
2025-06-24 12:25:25 -0300

    primer commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css'
+export const Navbar = () => {
+  return (
+    <nav className='labNav'>
+      <a href="#"><span>🧪</span> Portada</a>
+      <a href="#"><span>⚽</span> Juegos Olímpicos</a>
+      <a href="#"><span>🍳</span> Ciencia en Cocina</a>
+      <a href="#"><span>🤖</span> Tecnología</a>
+      <a href="#"><span>🌱</span> Ecología</a>
+      <a href="#"><span>🧠</span> Neurociencia</a>
+      <a href="#"><span>🛰️</span> Espacio</a>
+    </nav>
+  );
+};
